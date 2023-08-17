4efeb90c76da4a8ac04d4fcd598c234bcca8a87c
ravi-av <ravisrivastav2609@gmail.com>
2023-08-17 08:12:09 +0530

    basic codes

---
+++ Amazon-clone/script.js
@@ -0,0 +1,27 @@
+let slideBtnLeft = document.getElementById(" slide-btn-left")
+let slideBtnRight = document.getElementById("slide-btn-right")
+let imgItem = document.querySelectorAll(".image-item ")
+
+console.log(imgItem.length-1)
+ 
+let startSlider = 0
+let endSlider = imgItem.length-1
+
+slideBtnLeft.addEventListener("click",()=>{
+    alert("left btn")
+})
+
+slideBtnRight.addEventListener("click",()=>{
+  // if(startSlider >= -endSlider+100){
+    startSlider = startSlider - 100;
+  // }
+  
+imgItem.forEach(element =>{
+  element.style.transform =  'translateX(${startSlider}%)';
+
+})
+
+})
+
+
+
