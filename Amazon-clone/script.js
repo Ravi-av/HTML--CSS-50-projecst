2d31fb5e38864c59623261a3470c8d5bb5111a5e
ravi-av <ravisrivastav2609@gmail.com>
2023-09-04 22:02:46 +0530

    update

--- Amazon-clone/script.js
+++ Amazon-clone/script.js
@@ -1,27 +1,19 @@
-let slideBtnLeft = document.getElementById("slide-btn-left")
-let slideBtnRight = document.getElementById("slide-btn-right")
-let imgItem = document.querySelectorAll(".image-item")
+let slideBtnLeft = document.getElementById("slide-btn-left");
+let slideBtnRight = document.getElementById("slide-btn-right");
+let imgItem = document.querySelectorAll(".image-item");
 
-console.log(imgItem.length-1)
- 
-let startSlider = 0
-let endSlider = imgItem.length-1
+console.log(imgItem.length-1);
+// console.log(imgItem.length-1)
 
-slideBtnLeft.addEventListener("click",()=>{
-    alert("left btn")
-})
-
+let startSlider = 0;
+let endSlider = imgItem.length-1;
 slideBtnRight.addEventListener("click",()=>{
-  // if(startSlider >= -endSlider+100){
-    startSlider = startSlider +1;
-  // }
+startSlider=startSlider-100;
   
+
 imgItem.forEach(element =>{
-  element.style.transform =  'translateX(${startSlider}%)';
+ // Here i gonna change some single qoute   
+  element.style.transform = `translateX(${startSlider}%)`;
 
 })
-
 })
-
-
-
